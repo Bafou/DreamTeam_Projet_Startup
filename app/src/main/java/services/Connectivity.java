0446d2e7c275336d641f1e1fb07eb7499cea85cd
LucasDelvallet <lucas.delvallet@hotmail.fr>
2016-10-17 13:25:04 +0200

    It's now possible to get the current location of the device.

---
+++ app/src/main/java/services/Connectivity.java
@@ -0,0 +1,55 @@
+package services;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * Check device's network connectivity and speed
+ *
+ *
+ * @author emil http://stackoverflow.com/users/220710/emil
+ * @author Lucas Delvallet https://github.com/LucasDelvallet
+ */
+public class Connectivity {
+
+    /**
+     * Get the network info
+     * @param context
+     * @return The objet which contains various information about the device actual network
+     */
+    public static NetworkInfo getNetworkInfo(Context context){
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        return cm.getActiveNetworkInfo();
+    }
+
+    /**
+     * Check if there is any connectivity
+     * @param context
+     * @return true if there is, false if not
+     */
+    public static boolean isConnected(Context context){
+        NetworkInfo info = Connectivity.getNetworkInfo(context);
+        return (info != null && info.isConnected());
+    }
+
+    /**
+     * Check if there is any connectivity to a Wifi network
+     * @param context
+     * @return true if there is, false if not
+     */
+    public static boolean isConnectedWifi(Context context){
+        NetworkInfo info = Connectivity.getNetworkInfo(context);
+        return (info != null && info.isConnected() && info.getType() == ConnectivityManager.TYPE_WIFI);
+    }
+
+    /**
+     * Check if there is any connectivity to a mobile network
+     * @param context
+     * @return true if there is, false if not
+     */
+    public static boolean isConnectedMobile(Context context){
+        NetworkInfo info = Connectivity.getNetworkInfo(context);
+        return (info != null && info.isConnected() && info.getType() == ConnectivityManager.TYPE_MOBILE);
+    }
+}
