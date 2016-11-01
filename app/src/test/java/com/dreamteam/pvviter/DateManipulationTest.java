a6e06f6249978de5d2758a4e1634e52558d907ce
Florian Doublet <florian.doublet@hotmail.fr>
2016-11-01 09:16:00 +0100

    Set the end time of the timestamp manually (#15)
    * Add the possibility to directly set the time end of the timestamp manually

---
+++ app/src/test/java/com/dreamteam/pvviter/DateManipulationTest.java
@@ -0,0 +1,59 @@
+package com.dreamteam.pvviter;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.util.Calendar;
+import java.util.Hashtable;
+
+import utils.DateManipulation;
+
+/**
+ * Created by FlorianDoublet on 30/10/2016.
+ */
+public class DateManipulationTest {
+
+    @Test
+    public void isTomorrowTest() throws Exception{
+        //create a calendar to current date and add 1 day
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_MONTH,1);
+        //then check
+        assertTrue(DateManipulation.isTomorrow(calendar));
+    }
+
+    @Test
+    public void dateHourMinuteToString() throws Exception{
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, 14);
+        calendar.set(Calendar.MINUTE, 2);
+        String expectedString = "14:02";
+        assertEquals(expectedString, DateManipulation.dateHourMinuteToString(calendar.getTime()));
+    }
+
+    @Test
+    public void diffBetweenTwoDate() throws Exception{
+        Calendar oldCal = Calendar.getInstance();
+        oldCal.set(Calendar.DAY_OF_MONTH, 2);
+        oldCal.set(Calendar.HOUR_OF_DAY, 2);
+        oldCal.set(Calendar.MINUTE, 2);
+
+        //set a call with the new parameters for hours and minutes
+        Calendar newCal = Calendar.getInstance();
+        newCal.set(Calendar.DAY_OF_MONTH, 5);
+        newCal.set(Calendar.HOUR_OF_DAY, 6);
+        newCal.set(Calendar.MINUTE, 7);
+
+        //calculus the difference and set the result into the variable above
+        Hashtable<Integer, Integer> res = DateManipulation.diffBetweenTwoDate(newCal, oldCal);
+
+        int elapsedDays = res.get(DateManipulation.ELAPSED_DAYS);
+        int elapsedHours = res.get(DateManipulation.ELAPSED_HOURS);
+        int elapsedMinutes = res.get(DateManipulation.ELAPSED_MINUTES);
+
+        int[] expectedValues = {3, 4, 5};
+        int[] obtainedValue = {elapsedDays, elapsedHours, elapsedMinutes};
+
+        assertArrayEquals(expectedValues, obtainedValue);
+
+    }
+}
