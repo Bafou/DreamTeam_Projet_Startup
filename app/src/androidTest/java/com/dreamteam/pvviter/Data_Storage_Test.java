00bfaf52dace55955ce427e2030d7707f9865493
LucasDelvallet <lucas.delvallet@hotmail.fr>
2016-11-16 20:52:47 +0100

    Reseting the save files is now possible TG-47

--- app/src/androidTest/java/com/dreamteam/pvviter/Data_Storage_Test.java
+++ app/src/androidTest/java/com/dreamteam/pvviter/Data_Storage_Test.java
@@ -6,6 +6,9 @@ import android.test.ApplicationTestCase;
 import org.junit.Test;
 import org.osmdroid.util.GeoPoint;
 
+import java.io.File;
+
+import services.File_IO;
 import utils.Data_Storage;
 
 /**
@@ -47,5 +50,18 @@ public class Data_Storage_Test  extends ApplicationTestCase<Application> {
         long retrievedEndTime = Data_Storage.get_parking_end_time_in_milliseconds(mContext);
 
         assertEquals(endTime, retrievedEndTime);
+    }
+
+    @Test
+    public void test_file_deletion() {
+        Data_Storage.set_parking_end_time_in_milliseconds(mContext, 123);
+        Data_Storage.set_car_location(mContext, new GeoPoint(9.3, 3.11));
+        Data_Storage.set_user_location(mContext, new GeoPoint(9.3, 3.11));
+
+        File_IO.delete_all_files(mContext);
+
+        assertFalse(File_IO.does_file_exist(mContext, File_IO.PARKING_END_TIME));
+        assertFalse(File_IO.does_file_exist(mContext, File_IO.CAR_LOCATION_FILE));
+        assertFalse(File_IO.does_file_exist(mContext, File_IO.USER_LOCATION_FILE));
     }
 }
